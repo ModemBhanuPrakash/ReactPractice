d0ffd3b12ef361be7d74d9e8ff65ec2f0af01e8b
M.Bhanu Prakash <modembhanuprakash@gmail.com>
2024-06-18 11:55:15 +0530

    CSS Styling in React

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react'
+// import './App.css';
 import ClassComponent from './ClassComponent'
 import FunctionalComponent from './FunctionalComponent'
 import StateExampleInClassComponent from './StateExampleInClassComponent'
 import PropsExampleInClassComponent from './PropsExampleInClassComponent'
 const App = () => {
+  const styling={
+    color : "black",
+    backgroundColor : "lightgrey",
+    padding : "50px",
+    textAlign : "center"
+  }
   return (
-    <div>
-      <h1>My First React App</h1>
-      <ClassComponent></ClassComponent>
+    <div className='container'>
+      <h1 style={styling}>My First React App</h1>
+      {/*<ClassComponent></ClassComponent>
       <FunctionalComponent></FunctionalComponent>
       <StateExampleInClassComponent></StateExampleInClassComponent>
-      {/* <PropsExampleInClassComponent/>  */}
+       <PropsExampleInClassComponent/>  */}
     </div>
   )
 }
