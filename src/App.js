a5386f49a8122bcd341fab3a54adcd70c349c57d
M.Bhanu Prakash <modembhanuprakash@gmail.com>
2024-06-18 12:44:47 +0530

    Implemented counter button using onClick and useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React,{ useState } from 'react'
+
 // import './App.css';
 import ClassComponent from './ClassComponent'
 import FunctionalComponent from './FunctionalComponent'
 import StateExampleInClassComponent from './StateExampleInClassComponent'
 import PropsExampleInClassComponent from './PropsExampleInClassComponent'
+
 const App = () => {
   const styling={
     color : "black",
@@ -11,9 +13,14 @@ const App = () => {
     padding : "50px",
     textAlign : "center"
   }
+  const [count,setCount] = useState(0);
   return (
     <div className='container'>
       <h1 style={styling}>My First React App</h1>
+      <h2 style={{color:"black",textAlign:"center "}}>The No of times button clicked {count}</h2>
+      <center>
+      <button style={{color:"black",backgroundColor:"lightgrey",padding:"10px",margin:"50px"}} onClick={() => setCount(count+1)}>Click here</button>
+      </center>
       {/*<ClassComponent></ClassComponent>
       <FunctionalComponent></FunctionalComponent>
       <StateExampleInClassComponent></StateExampleInClassComponent>
