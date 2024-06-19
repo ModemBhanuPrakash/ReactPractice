dea4553c92cd9615beb72da5f6c41ad36341cf19
M.Bhanu Prakash <modembhanuprakash@gmail.com>
2024-06-19 12:10:41 +0530

    understood filter function in reactjs

--- src/Map.js
+++ src/Map.js
@@ -20,9 +20,13 @@ const Map = () => {
             name :'lohith'
         }
     ]
+    const fruits = ['apple','banana','pineapple','leach','mango']
+    const filteredFruits = fruits.filter(fruit => fruit.includes('e'))
+    const numbers = [1,20,-6,65,85,-96,12,-74,-39,301]
+    const posNumbers = numbers.filter(num => num>=0)
   return (
     <div>
-      {
+      {/* {
         arr.map(
             (value,index) => <li key={index}>{value}</li>
         )
@@ -31,6 +35,16 @@ const Map = () => {
       {
          arr1.map(
             (value) => <li key={value.id}>{value.id} : {value.name}</li>
+        )
+      } */}
+      {
+        filteredFruits.map(
+            (fruit) => <li key={fruit}>{fruit}</li>
+        )
+      }
+      {
+        posNumbers.map(
+            (num,index) => <li key={index}>{num}</li>
         )
       }
     </div>
