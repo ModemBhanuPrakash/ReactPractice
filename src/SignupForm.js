4cbca604fd07c60ea763490b53c9df4df321d569
M.Bhanu Prakash <modembhanuprakash@gmail.com>
2024-06-19 16:50:41 +0530

    Form validation

--- src/SignupForm.js
+++ src/SignupForm.js
@@ -15,23 +15,29 @@ const SignupForm = () => {
     }
     const submitHandler = e => {
         e.preventDefault()
-        if(password==confirmPassword){
-            console.log(data)
+        if(username.length<5){
+            alert("username must have atleast 5 characters")
+        }
+        else if(mobile.length!==10){
+            alert("Enter valid mobile number")
+        }
+        else if(password !== confirmPassword){
+            alert("Passwords are not matching")
         }
         else{
-            console.log("password and confirm password are not matching")
+            console.log(data)
         }
     }
   return (
     <div>
       <center>
         <form onSubmit={submitHandler}>
-            <input type='text' name='username' value={username} onChange={formHandler}/> <br/>
-            <input type='email' name='email' value={email} onChange={formHandler}/>  <br/>
-            <input type='text' name='mobile' value={mobile} onChange={formHandler}/>  <br/>
-            <input type='text' name='address' value={address} onChange={formHandler}/> <br/>
-            <input type='password' name='password' value={password} onChange={formHandler}/> <br/>
-            <input type='password' name='confirmPassword' value={confirmPassword} onChange={formHandler}/> <br/>
+            <input type='text' name='username' value={username} onChange={formHandler} placeholder='Enter Username'/> <br/>
+            <input type='email' name='email' value={email} onChange={formHandler} placeholder='Enter EmailID'/>  <br/>
+            <input type='text' name='mobile' value={mobile} onChange={formHandler} placeholder='Enter Mobile No'/>  <br/>
+            <input type='text' name='address' value={address} onChange={formHandler} placeholder='Enter Address'/> <br/>
+            <input type='password' name='password' value={password} onChange={formHandler} placeholder='Enter Password'/> <br/>
+            <input type='password' name='confirmPassword' value={confirmPassword} onChange={formHandler} placeholder='Confirm Password'/> <br/>
             <button>Submit</button>
         </form>
       </center>
